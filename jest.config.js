d77741c680a1614b814ed04fde12ba24496301ac
teejay4125 <teejay4125@gmail.com>
2025-01-01 15:00:45 +0000

    update

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {
+        "^.+\\.jsx?$": "babel-jest",
+    },
+    moduleNameMapper: {
+        "\\.(css|scss)$": "identity-obj-proxy",
+    },
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["js", "jsx"],
+    setupFilesAfterEnv: ["./src/setupTests.js"],
+}
